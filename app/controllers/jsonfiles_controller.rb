aeafb6fc483e016e4ff8268a26a359cab66eaed5
tingding96 <tingding96@gmail.com>
2019-03-29 01:03:43 -0700

    added new functionality, but not showing title

--- app/controllers/jsonfiles_controller.rb
+++ app/controllers/jsonfiles_controller.rb
@@ -1,17 +1,18 @@
 class JsonfilesController < ApplicationController
 
-  def movie_params
-    params.require(:jsonfile).permit(:title, :file)
+  def jsonfile_params
+    params.require(:jsonfile).permit(:title, :content)
   end
 
   #To show the data fields and their relationships in a json data file.
   def show
-    render "show"
+    id = params[:id] # retrieve movie ID from URI route
+    @jsonfile = Jsonfile.find(id) # look up movie by unique ID
   end
 
   #To show the list of json files uploaded to the application.
   def index
-    # @jsonfile = Jsonfile.all
+    @jsonfiles = Jsonfile.all
   end
 
   # default: render 'new' template
@@ -21,13 +22,13 @@ class JsonfilesController < ApplicationController
 
   #To create the jsonfile and store it into our database.
   def create
-    @file = params[:file].read
+    @file = params[:content].read
     @title = params[:title]
-    @data = JSON.parse(file)
-    @jsonfile = Jsonfile.create({:title => @title, :content => @data})
+    @data = JSON.parse(@file)
+    @jsonfile = Jsonfile.create!({:title => @title, :content => @data})
+    @jsonfile.content = @data
     flash[:notice] = "#{@jsonfile.title} was successfully created."
     redirect_to jsonfiles_path
-    # redirect_to jsonfiles_path
   end
 
   #To edit the jsonfile, but not really necessary at the moment.
@@ -40,6 +41,10 @@ class JsonfilesController < ApplicationController
 
   #To delete the jsonfile from database.
   def destroy
+    @jsonfile = Jsonfile.find(params[:id])
+    @jsonfile.destroy
+    flash[:notice] = "Json File '#{@mjsonfile.title}' deleted."
+    redirect_to jsonfiles_path
   end
 
 end
